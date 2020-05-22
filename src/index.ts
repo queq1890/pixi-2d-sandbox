db1a85856f10d302cc04a1bd66d75f08f5e3b436
Yuji Matusmoto (queq1890) <yuji.matsumoto@sun-asterisk.com>
2020-05-22 10:39:03 +0700

    set up minimum env

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,16 @@
-const foo = ['aaa', ''];
+import 'normalize.css';
+import './index.css';
+import { Application } from 'pixi.js';
+
+const app = new Application({
+  width: window.innerWidth,
+  height: window.innerHeight,
+  backgroundColor: 0xff00ff,
+});
+
+const resize = () => {
+  app.renderer.resize(window.innerWidth, window.innerHeight);
+};
+
+document.body.append(app.view);
+window.addEventListener('resize', resize);
--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,13 @@ const path = require('path');
 module.exports = {
   entry: './src/index.ts',
   module: {
-    rules: [{ test: /\.tsx$/, use: 'ts-loader', exclude: /node_modules/ }],
+    rules: [
+      { test: /\.tsx$/, use: 'ts-loader', exclude: /node_modules/ },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
   },
   resolve: {
     extensions: ['.tsx', '.ts', '.js'],
